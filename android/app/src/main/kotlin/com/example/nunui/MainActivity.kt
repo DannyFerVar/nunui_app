35297adbcded9a9c7fd84814327f59701f562370
Daniela Fernández Varela <danny.fervar@gmail.com>
2022-06-29 20:33:17 -0500

    final application generated

---
+++ android/app/src/main/kotlin/com/example/nunui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nunui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NunuiApp());
+}
+
+class NunuiApp extends StatelessWidget {
+  const NunuiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
